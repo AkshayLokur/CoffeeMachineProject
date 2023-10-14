7b8881dc6f41556f31a65d4802c4f2f503f80d7d
Akshay Lokur <alokur@apple.com>
2023-10-14 14:59:43 +0800

    Money calculation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from menu import MENU
 from menu import resources as machine_resources
 
 money_earned = 0
+QUARTER = 0.25
+DIME = 0.10
+NICKEL = 0.05
+PENNY = 0.01
 
 
 def print_report():
@@ -12,17 +16,30 @@ def print_report():
     print(f"Money: ${money_earned}")
 
 
-def process_coins(cost):
-    pass
+def receive_money():
+    quarters = int(input("Enter quarters: "))
+    dimes = int(input("Enter dimes: "))
+    nickels = int(input("Enter nickels: "))
+    pennies = int(input("Enter pennies: "))
+
+    money_received_from_user = round(quarters * QUARTER + dimes * DIME + nickels * NICKEL + pennies * PENNY, 2)
+    print(f"Money received: ${money_received_from_user}")
+
+    return money_received_from_user
 
 
-def make_coffee(coffee_type, water_req, milk_req, coffee_req, cost):
+def return_change(money_received, cost_of_coffee):
+    if money_received > cost_of_coffee:
+        print(f"Here is ${round(money_received - cost_of_coffee, 2)} dollars in change.")
+
+
+def make_coffee(coffee_type, water_req, milk_req, coffee_req, cost_of_coffee):
     global money_earned
 
     machine_resources["water"] -= water_req
     machine_resources["milk"] -= milk_req
     machine_resources["coffee"] -= coffee_req
-    money_earned += cost
+    money_earned += cost_of_coffee
 
     print(f"Enjoy your {coffee_type}! ☕️")
 
@@ -45,13 +62,17 @@ def are_resources_sufficient(coffee_type):
         water_req = coffee_menu["ingredients"]["water"] if "water" in coffee_menu["ingredients"] else 0
         milk_req = coffee_menu["ingredients"]["milk"] if "milk" in coffee_menu["ingredients"] else 0
         coffee_req = coffee_menu["ingredients"]["coffee"] if "coffee" in coffee_menu["ingredients"] else 0
-        cost = coffee_menu["cost"]
+        cost_of_coffee = coffee_menu["cost"]
 
         if is_resource_sufficient("milk", milk_req) and \
                 is_resource_sufficient("coffee", coffee_req) and \
                 is_resource_sufficient("water", water_req):
-            process_coins(cost)
-            make_coffee(coffee_type, water_req, milk_req, coffee_req, cost)
+            money_received = receive_money()
+            if money_received < cost_of_coffee:
+                print("Sorry that's not enough money. Money refunded")
+            else:
+                return_change(money_received, cost_of_coffee)
+                make_coffee(coffee_type, water_req, milk_req, coffee_req, cost_of_coffee)
         else:
             print(f"Sorry! Not enough resources to make a {coffee_type}")
 
